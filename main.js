881ff59dee56a1cd1b27e78a0a5f788633560c9b
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2021-11-09 16:40:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,39 @@ function setup()
     canvas=createCanvas(300,300);
 canvas.center();
 background("white");
+canvas.mouseReleased(classifycanvas);
+synth=window.speechSynthesis;
 }
 function clearcanvas()
 {
 background("white");
-}   
+}
+function preload()  
+{
+    classifier=ml5.imageClassifier('DoodleNet');
+} 
+function draw()
+{
+strokeWeight(5);
+stroke("blue");
+if(mouseIsPressed)
+{
+  line(pmouseX,pmouseY,mouseX,mouseY);
+}
+}
+function classifycanvas()
+{
+    classifier.classify(canvas,gotresult);
+}
+function gotresult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML="Label: "+results[0].label;
+    document.getElementById("confidence").innerHTML="confidence: "+Math.round(results[0].confidence*100)+"%";
+utterThis=new SpeechSynthesisUtterance(results[0].label);
+synth.speak(utterThis);
+}
